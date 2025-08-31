9d0b4065adb299be7f94a59045d912158aec8ec1
Fati Iseni <fati.iseni@pozitrongroup.com>
2025-09-01 00:21:00 +0300

    Add RSA key generator.

---
+++ src/NuSeal.Generator/RsaKeyGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+
+namespace NuSeal;
+
+public class RsaKeyGenerator
+{
+    public static RsaPemPair GeneratePem()
+    {
+        using var rsa = RSA.Create(2048);
+        var privateKey = rsa.ExportPkcs8PrivateKeyPem();
+        var publicKey = rsa.ExportSubjectPublicKeyInfoPem();
+        return new RsaPemPair(publicKey, privateKey);
+    }
+}
+
+public class RsaPemPair(string publicKey, string privateKey)
+{
+    public string PublicKey { get; } = publicKey;
+    public string PrivateKey { get; } = privateKey;
+}
