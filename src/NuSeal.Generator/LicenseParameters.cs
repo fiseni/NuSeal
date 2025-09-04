5ae39b9a943c69c19b639c1bddbce164ff70bb4d
Fati Iseni <fati.iseni@pozitrongroup.com>
2025-09-04 23:24:12 +0300

    Add optional `client` claim to licenses. (#10)

--- src/NuSeal.Generator/LicenseParameters.cs
+++ src/NuSeal.Generator/LicenseParameters.cs
@@ -9,6 +9,7 @@ public record LicenseParameters
     public string Issuer { get; init; } = "NuSeal";
     public string Audience { get; init; } = "NuSeal";
     public string SubscriptionId { get; init; } = Guid.Empty.ToString();
+    public string? ClientId { get; init; }
     public string? Edition { get; init; }
     public DateTimeOffset StartDate { get; init; } = DateTimeOffset.UtcNow;
     public DateTimeOffset ExpirationDate { get; init; } = DateTimeOffset.UtcNow.AddYears(1);
