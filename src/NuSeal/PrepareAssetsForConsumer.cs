ad736594f750d1fc1a669607e6122e47b92db093
Fati Iseni <fati.iseni@pozitrongroup.com>
2025-09-06 22:01:06 +0300

    Extract task for easier testing.

---
+++ src/NuSeal/PrepareAssetsForConsumer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace NuSeal;
+
+internal class PrepareAssetsForConsumer
+{
+    public static bool Execute(
+        string nusealAssetsPath,
+        string nusealVersion,
+        string outputPath,
+        string consumerPackageId,
+        string consumerPropsFile,
+        string consumerTargetsFile)
+    {
+        //Debugger.Launch();
+
+        var nusealPropsFile = Path.Combine(nusealAssetsPath, "NuSeal.Direct.props");
+        var nusealTargetsFile = Path.Combine(nusealAssetsPath, "NuSeal.Direct.targets");
+        var propsOutputFile = Path.Combine(outputPath, $"{consumerPackageId}.props");
+        var targetsOutputFile = Path.Combine(outputPath, $"{consumerPackageId}.targets");
+
+        var props = File.ReadAllText(nusealPropsFile);
+        var targets = File.ReadAllText(nusealTargetsFile);
+
+        props = props.Replace("_NuSealVersion_", nusealVersion);
+
+        if (!string.IsNullOrEmpty(consumerPropsFile) && File.Exists(consumerPropsFile))
+        {
+            var content = File.ReadAllText(consumerPropsFile);
+            content = RemoveProjectTags(content);
+            props = props.Replace("</Project>", $"{content}{Environment.NewLine}</Project>");
+        }
+
+        if (!string.IsNullOrEmpty(consumerTargetsFile) && File.Exists(consumerTargetsFile))
+        {
+            var content = File.ReadAllText(consumerTargetsFile);
+            content = RemoveProjectTags(content);
+            targets = targets.Replace("</Project>", $"{content}{Environment.NewLine}</Project>");
+        }
+
+        File.WriteAllText(propsOutputFile, props);
+        File.WriteAllText(targetsOutputFile, targets);
+
+        return true;
+    }
+
+    // Very rudimentary, but it's not worth parsing the XML properly for this
+    private static string RemoveProjectTags(string content)
+    {
+        int startIndex = content.IndexOf("<Project");
+        if (startIndex == -1)
+            return content;
+        int endIndex = content.IndexOf(">", startIndex);
+        if (endIndex == -1)
+            return content;
+        string projectTag = content.Substring(startIndex, endIndex - startIndex + 1);
+        return content.Replace(projectTag, "").Replace("</Project>", "");
+    }
+}
--- src/NuSeal/PrepareAssetsForConsumerTask.cs
+++ src/NuSeal/PrepareAssetsForConsumerTask.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 using System;
-using System.IO;
 
 namespace NuSeal;
 
@@ -25,56 +24,21 @@ public partial class PrepareAssetsForConsumerTask : Task
 
     public override bool Execute()
     {
-        //Debugger.Launch();
-
         try
         {
-            var nusealPropsFile = Path.Combine(NuSealAssetsPath, "NuSeal.Direct.props");
-            var nusealTargetsFile = Path.Combine(NuSealAssetsPath, "NuSeal.Direct.targets");
-            var propsOutputFile = Path.Combine(OutputPath, $"{ConsumerPackageId}.props");
-            var targetsOutputFile = Path.Combine(OutputPath, $"{ConsumerPackageId}.targets");
+            return PrepareAssetsForConsumer.Execute(
+                NuSealAssetsPath,
+                NuSealVersion,
+                OutputPath,
+                ConsumerPackageId,
+                ConsumerPropsFile,
+                ConsumerTargetsFile);
 
-            var props = File.ReadAllText(nusealPropsFile);
-            var targets = File.ReadAllText(nusealTargetsFile);
-
-            props = props.Replace("_NuSealVersion_", NuSealVersion);
-
-            if (!string.IsNullOrEmpty(ConsumerPropsFile) && File.Exists(ConsumerPropsFile))
-            {
-                var content = File.ReadAllText(ConsumerPropsFile);
-                content = RemoveProjectTags(content);
-                props = props.Replace("</Project>", $"{content}{Environment.NewLine}</Project>");
-            }
-
-            if (!string.IsNullOrEmpty(ConsumerTargetsFile) && File.Exists(ConsumerTargetsFile))
-            {
-                var content = File.ReadAllText(ConsumerTargetsFile);
-                content = RemoveProjectTags(content);
-                targets = targets.Replace("</Project>", $"{content}{Environment.NewLine}</Project>");
-            }
-
-            File.WriteAllText(propsOutputFile, props);
-            File.WriteAllText(targetsOutputFile, targets);
-
-            return true;
         }
         catch (Exception ex)
         {
             Log.LogErrorFromException(ex, true);
             return false;
         }
-    }
-
-    // Very rudimentary, but it's not worth parsing the XML properly for this
-    private static string RemoveProjectTags(string content)
-    {
-        int startIndex = content.IndexOf("<Project");
-        if (startIndex == -1)
-            return content;
-        int endIndex = content.IndexOf(">", startIndex);
-        if (endIndex == -1)
-            return content;
-        string projectTag = content.Substring(startIndex, endIndex - startIndex + 1);
-        return content.Replace(projectTag, "").Replace("</Project>", "");
     }
 }
