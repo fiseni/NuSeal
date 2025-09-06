0e369c85110faa164065e9a7815abba76e023370
Fati Iseni <fati.iseni@pozitrongroup.com>
2025-09-06 22:45:45 +0300

    Add tests for AssemblyUtils.

---
+++ tests/NuSeal.Tests/AssemblyUtils_ExtractOptionsTests.cs
@@ -0,0 +1,213 @@
+﻿using Mono.Cecil;
+
+namespace Tests;
+
+public class AssemblyUtils_ExtractOptionsTests : IDisposable
+{
+    private AssemblyDefinition _testAssembly;
+
+    public AssemblyUtils_ExtractOptionsTests()
+    {
+        // Create a simple assembly definition for testing
+        var assemblyName = new AssemblyNameDefinition("TestAssembly", new Version(1, 0, 0, 0));
+        _testAssembly = AssemblyDefinition.CreateAssembly(
+            assemblyName,
+            "TestModule",
+            ModuleKind.Dll);
+    }
+
+    [Fact]
+    public void ReturnsDefaultOptions_GivenAssemblyWithNoAttributes()
+    {
+        // Act
+        var result = AssemblyUtils.ExtractOptions(_testAssembly);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.IsProtected.Should().BeFalse();
+        result.ValidationBehavior.Should().Be(NuSealValidationBehavior.Error);
+        result.TransitiveBehavior.Should().Be(NuSealTransitiveBehavior.Enabled);
+    }
+
+    [Fact]
+    public void SetsIsProtected_GivenAssemblyWithProtectedAttribute()
+    {
+        // Arrange
+        AddCustomAttribute(_testAssembly, typeof(NuSealProtectedAttribute).Name);
+
+        // Act
+        var result = AssemblyUtils.ExtractOptions(_testAssembly);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.IsProtected.Should().BeTrue();
+        result.ValidationBehavior.Should().Be(NuSealValidationBehavior.Error); // Default
+        result.TransitiveBehavior.Should().Be(NuSealTransitiveBehavior.Enabled); // Default
+    }
+
+    [Fact]
+    public void SetsWarningValidationBehavior_GivenAssemblyWithWarningAttribute()
+    {
+        // Arrange
+        AddCustomAttributeWithValue(_testAssembly, typeof(NuSealValidationBehaviorAttribute).Name, "Warning");
+
+        // Act
+        var result = AssemblyUtils.ExtractOptions(_testAssembly);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.IsProtected.Should().BeFalse(); // Default
+        result.ValidationBehavior.Should().Be(NuSealValidationBehavior.Warning);
+        result.TransitiveBehavior.Should().Be(NuSealTransitiveBehavior.Enabled); // Default
+    }
+
+    [Fact]
+    public void KeepsDefaultValidationBehavior_GivenAssemblyWithInvalidValidationBehavior()
+    {
+        // Arrange
+        AddCustomAttributeWithValue(_testAssembly, typeof(NuSealValidationBehaviorAttribute).Name, "InvalidValue");
+
+        // Act
+        var result = AssemblyUtils.ExtractOptions(_testAssembly);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.ValidationBehavior.Should().Be(NuSealValidationBehavior.Error); // Default unchanged
+    }
+
+    [Fact]
+    public void SetsDisabledTransitiveBehavior_GivenAssemblyWithDisabledAttribute()
+    {
+        // Arrange
+        AddCustomAttributeWithValue(_testAssembly, typeof(NuSealTransitiveBehaviorAttribute).Name, "disable");
+
+        // Act
+        var result = AssemblyUtils.ExtractOptions(_testAssembly);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.IsProtected.Should().BeFalse(); // Default
+        result.ValidationBehavior.Should().Be(NuSealValidationBehavior.Error); // Default
+        result.TransitiveBehavior.Should().Be(NuSealTransitiveBehavior.Disabled);
+    }
+
+    [Fact]
+    public void KeepsDefaultTransitiveBehavior_GivenAssemblyWithInvalidTransitiveBehavior()
+    {
+        // Arrange
+        AddCustomAttributeWithValue(_testAssembly, typeof(NuSealTransitiveBehaviorAttribute).Name, "InvalidValue");
+
+        // Act
+        var result = AssemblyUtils.ExtractOptions(_testAssembly);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.TransitiveBehavior.Should().Be(NuSealTransitiveBehavior.Enabled); // Default unchanged
+    }
+
+    [Fact]
+    public void CombinesAllAttributes_GivenAssemblyWithMultipleAttributes()
+    {
+        // Arrange
+        AddCustomAttribute(_testAssembly, typeof(NuSealProtectedAttribute).Name);
+        AddCustomAttributeWithValue(_testAssembly, typeof(NuSealValidationBehaviorAttribute).Name, "Warning");
+        AddCustomAttributeWithValue(_testAssembly, typeof(NuSealTransitiveBehaviorAttribute).Name, "disable");
+
+        // Act
+        var result = AssemblyUtils.ExtractOptions(_testAssembly);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.IsProtected.Should().BeTrue();
+        result.ValidationBehavior.Should().Be(NuSealValidationBehavior.Warning);
+        result.TransitiveBehavior.Should().Be(NuSealTransitiveBehavior.Disabled);
+    }
+
+    [Fact]
+    public void IgnoresUnrelatedAttributes_GivenAssemblyWithMixedAttributes()
+    {
+        // Arrange
+        AddCustomAttribute(_testAssembly, typeof(NuSealProtectedAttribute).Name);
+        AddCustomAttribute(_testAssembly, "SomeOtherAttribute");
+
+        // Act
+        var result = AssemblyUtils.ExtractOptions(_testAssembly);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.IsProtected.Should().BeTrue();
+        result.ValidationBehavior.Should().Be(NuSealValidationBehavior.Error); // Default
+        result.TransitiveBehavior.Should().Be(NuSealTransitiveBehavior.Enabled); // Default
+    }
+
+    [Fact]
+    public void HandlesCaseInsensitively_GivenAttributeValuesWithDifferentCase()
+    {
+        // Arrange
+        AddCustomAttributeWithValue(_testAssembly, typeof(NuSealValidationBehaviorAttribute).Name, "warning"); // lowercase
+        AddCustomAttributeWithValue(_testAssembly, typeof(NuSealTransitiveBehaviorAttribute).Name, "DISABLE"); // uppercase
+
+        // Act
+        var result = AssemblyUtils.ExtractOptions(_testAssembly);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.ValidationBehavior.Should().Be(NuSealValidationBehavior.Warning);
+        result.TransitiveBehavior.Should().Be(NuSealTransitiveBehavior.Disabled);
+    }
+
+    private static void AddCustomAttribute(AssemblyDefinition assembly, string attributeTypeName)
+    {
+        var moduleRef = new ModuleReference("System.Runtime");
+        assembly.MainModule.ModuleReferences.Add(moduleRef);
+
+        var attributeType = new TypeReference(
+            "NuSeal",
+            attributeTypeName,
+            assembly.MainModule,
+            moduleRef);
+
+        var attributeCtor = new MethodReference(
+            ".ctor",
+            assembly.MainModule.TypeSystem.Void,
+            attributeType);
+
+        attributeCtor.Parameters.Clear();
+        attributeCtor.HasThis = true;
+
+        var attribute = new CustomAttribute(attributeCtor);
+        assembly.CustomAttributes.Add(attribute);
+    }
+
+    private static void AddCustomAttributeWithValue(AssemblyDefinition assembly, string attributeTypeName, string value)
+    {
+        var moduleRef = new ModuleReference("System.Runtime");
+        assembly.MainModule.ModuleReferences.Add(moduleRef);
+
+        var attributeType = new TypeReference(
+            "NuSeal",
+            attributeTypeName,
+            assembly.MainModule,
+            moduleRef);
+
+        var attributeCtor = new MethodReference(
+            ".ctor",
+            assembly.MainModule.TypeSystem.Void,
+            attributeType);
+
+        var stringType = assembly.MainModule.TypeSystem.String;
+        attributeCtor.Parameters.Add(new ParameterDefinition(stringType));
+        attributeCtor.HasThis = true;
+
+        var attribute = new CustomAttribute(attributeCtor);
+        attribute.ConstructorArguments.Add(
+            new CustomAttributeArgument(stringType, value));
+
+        assembly.CustomAttributes.Add(attribute);
+    }
+
+    public void Dispose()
+    {
+        _testAssembly?.Dispose();
+    }
+}
---
+++ tests/NuSeal.Tests/AssemblyUtils_ExtractPemsTests.cs
@@ -0,0 +1,243 @@
+﻿using Mono.Cecil;
+
+namespace Tests;
+
+public class AssemblyUtils_ExtractPemsTests : IDisposable
+{
+    private readonly string _testDirectory;
+    private readonly string _testAssemblyPath;
+    private AssemblyDefinition _testAssembly;
+
+    public AssemblyUtils_ExtractPemsTests()
+    {
+        // Create a unique test directory for each test run
+        _testDirectory = Path.Combine(Path.GetTempPath(), $"NuSealTests_{Guid.NewGuid()}");
+        Directory.CreateDirectory(_testDirectory);
+        _testAssemblyPath = Path.Combine(_testDirectory, "TestAssembly.dll");
+
+        // Create a simple assembly definition for testing
+        var assemblyName = new AssemblyNameDefinition("TestAssembly", new Version(1, 0, 0, 0));
+        _testAssembly = AssemblyDefinition.CreateAssembly(
+            assemblyName,
+            "TestModule",
+            ModuleKind.Dll);
+    }
+
+    [Fact]
+    public void ReturnsEmptyList_GivenAssemblyWithNoResources()
+    {
+        // Act
+        var result = AssemblyUtils.ExtractPems(_testAssembly);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void ReturnsEmptyList_GivenAssemblyWithNonPemResources()
+    {
+        // Arrange
+        var resource = new EmbeddedResource("TestResource.txt",
+            ManifestResourceAttributes.Public,
+            new byte[] { 1, 2, 3 });
+        _testAssembly.MainModule.Resources.Add(resource);
+
+        // Act
+        var result = AssemblyUtils.ExtractPems(_testAssembly);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void ReturnsEmptyList_GivenAssemblyWithPemResourceButInvalidNameFormat()
+    {
+        // Arrange
+        // Resource name doesn't have enough parts separated by dots
+        var resource = new EmbeddedResource("nuseal.pem",
+            ManifestResourceAttributes.Public,
+            GetUtf8Bytes("test content"));
+        _testAssembly.MainModule.Resources.Add(resource);
+
+        // Act
+        var result = AssemblyUtils.ExtractPems(_testAssembly);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void ReturnsPemData_GivenAssemblyWithValidPemResource()
+    {
+        // Arrange
+        string productName = "TestProduct";
+        string pemContent = "-----BEGIN PUBLIC KEY-----\nMIIB...etc\n-----END PUBLIC KEY-----";
+        var resource = new EmbeddedResource($"namespace.{productName}.nuseal.pem",
+            ManifestResourceAttributes.Public,
+            GetUtf8Bytes(pemContent));
+
+        _testAssembly.MainModule.Resources.Add(resource);
+
+        // Act
+        var result = AssemblyUtils.ExtractPems(_testAssembly);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().HaveCount(1);
+        result[0].ProductName.Should().Be(productName);
+        result[0].PublicKeyPem.Should().Be(pemContent);
+    }
+
+    [Fact]
+    public void ReturnsMultiplePemData_GivenAssemblyWithMultiplePemResources()
+    {
+        // Arrange
+        string product1 = "Product1";
+        string product2 = "Product2";
+        string pem1 = "PEM1 Content";
+        string pem2 = "PEM2 Content";
+
+        var resource1 = new EmbeddedResource($"namespace.{product1}.nuseal.pem",
+            ManifestResourceAttributes.Public,
+            GetUtf8Bytes(pem1));
+
+        var resource2 = new EmbeddedResource($"namespace.{product2}.nuseal.pem",
+            ManifestResourceAttributes.Public,
+            GetUtf8Bytes(pem2));
+
+        _testAssembly.MainModule.Resources.Add(resource1);
+        _testAssembly.MainModule.Resources.Add(resource2);
+
+        // Act
+        var result = AssemblyUtils.ExtractPems(_testAssembly);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().HaveCount(2);
+
+        var pemData1 = result.FirstOrDefault(p => p.ProductName == product1);
+        var pemData2 = result.FirstOrDefault(p => p.ProductName == product2);
+
+        pemData1.Should().NotBeNull();
+        pemData2.Should().NotBeNull();
+        pemData1?.PublicKeyPem.Should().Be(pem1);
+        pemData2?.PublicKeyPem.Should().Be(pem2);
+    }
+
+    [Fact]
+    public void ReturnsPemData_GivenResourceWithoutNamespace()
+    {
+        // Arrange
+        string productName = "TestProduct";
+        string pemContent = "PEM Content";
+
+        // Use uppercase in the suffix
+        var resource = new EmbeddedResource($"{productName}.nuseal.pem",
+            ManifestResourceAttributes.Public,
+            GetUtf8Bytes(pemContent));
+
+        _testAssembly.MainModule.Resources.Add(resource);
+
+        // Act
+        var result = AssemblyUtils.ExtractPems(_testAssembly);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().HaveCount(1);
+        result[0].ProductName.Should().Be(productName);
+        result[0].PublicKeyPem.Should().Be(pemContent);
+    }
+
+    [Fact]
+    public void IsCaseInsensitive_GivenResourceWithDifferentCase()
+    {
+        // Arrange
+        string productName = "TestProduct";
+        string pemContent = "PEM Content";
+
+        // Use uppercase in the suffix
+        var resource = new EmbeddedResource($"namespace.{productName}.NUSEAL.PEM",
+            ManifestResourceAttributes.Public,
+            GetUtf8Bytes(pemContent));
+
+        _testAssembly.MainModule.Resources.Add(resource);
+
+        // Act
+        var result = AssemblyUtils.ExtractPems(_testAssembly);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().HaveCount(1);
+        result[0].ProductName.Should().Be(productName);
+        result[0].PublicKeyPem.Should().Be(pemContent);
+    }
+
+    [Fact]
+    public void HandlesLongerResourceNames_GivenComplexResourceName()
+    {
+        // Arrange
+        string productName = "TestProduct";
+        string pemContent = "PEM Content";
+
+        // Use a longer resource name with more dots
+        var resource = new EmbeddedResource($"some.complex.namespace.{productName}.nuseal.pem",
+            ManifestResourceAttributes.Public,
+            GetUtf8Bytes(pemContent));
+
+        _testAssembly.MainModule.Resources.Add(resource);
+
+        // Act
+        var result = AssemblyUtils.ExtractPems(_testAssembly);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().HaveCount(1);
+        result[0].ProductName.Should().Be(productName);
+        result[0].PublicKeyPem.Should().Be(pemContent);
+    }
+
+    [Fact]
+    public void ExtractsPemContent_GivenResourceWithMultilineContent()
+    {
+        // Arrange
+        string productName = "TestProduct";
+        string pemContent = "Line1\nLine2\nLine3";
+
+        var resource = new EmbeddedResource($"namespace.{productName}.nuseal.pem",
+            ManifestResourceAttributes.Public,
+            GetUtf8Bytes(pemContent));
+
+        _testAssembly.MainModule.Resources.Add(resource);
+
+        // Act
+        var result = AssemblyUtils.ExtractPems(_testAssembly);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().HaveCount(1);
+        result[0].PublicKeyPem.Should().Be(pemContent);
+    }
+
+    private static byte[] GetUtf8Bytes(string text)
+    {
+        return System.Text.Encoding.UTF8.GetBytes(text);
+    }
+
+    public void Dispose()
+    {
+        try
+        {
+            if (Directory.Exists(_testDirectory))
+            {
+                Directory.Delete(_testDirectory, true);
+            }
+            _testAssembly?.Dispose();
+        }
+        catch
+        {
+        }
+    }
+}
