5792574d6009f5273ebfc766d8513a0ad86111a7
Fati Iseni <fati.iseni@pozitrongroup.com>
2025-09-07 00:30:20 +0300

    Minor fixes.

--- src/NuSeal/PrepareAssetsForConsumer.cs
+++ src/NuSeal/PrepareAssetsForConsumer.cs
@@ -29,14 +29,14 @@ internal class PrepareAssetsForConsumer
         if (!string.IsNullOrEmpty(consumerPropsFile) && File.Exists(consumerPropsFile))
         {
             var content = File.ReadAllText(consumerPropsFile);
-            content = RemoveProjectTags(content);
+            content = RemoveProjectTags(content, consumerPropsFile);
             props = props.Replace("</Project>", $"{content}{Environment.NewLine}</Project>");
         }
 
         if (!string.IsNullOrEmpty(consumerTargetsFile) && File.Exists(consumerTargetsFile))
         {
             var content = File.ReadAllText(consumerTargetsFile);
-            content = RemoveProjectTags(content);
+            content = RemoveProjectTags(content, consumerTargetsFile);
             targets = targets.Replace("</Project>", $"{content}{Environment.NewLine}</Project>");
         }
 
@@ -47,14 +47,20 @@ internal class PrepareAssetsForConsumer
     }
 
     // Very rudimentary, but it's not worth parsing the XML properly for this
-    private static string RemoveProjectTags(string content)
+    private static string RemoveProjectTags(string content, string fileName)
     {
-        int startIndex = content.IndexOf("<Project");
+        var startIndex = content.IndexOf("<Project");
         if (startIndex == -1)
-            return content;
-        int endIndex = content.IndexOf(">", startIndex);
+            throw new ArgumentException($"The provided content does not contain a <Project> tag. File {fileName}");
+
+        var endIndex = content.IndexOf(">", startIndex);
         if (endIndex == -1)
-            return content;
+            throw new ArgumentException($"The provided content has invalid xml content. File {fileName}");
+
+        var closingTagIndex = content.IndexOf("</Project>", endIndex);
+        if (closingTagIndex == -1)
+            throw new ArgumentException($"The provided content does not contain a closing </Project> tag. File {fileName}");
+
         string projectTag = content.Substring(startIndex, endIndex - startIndex + 1);
         return content.Replace(projectTag, "").Replace("</Project>", "");
     }
