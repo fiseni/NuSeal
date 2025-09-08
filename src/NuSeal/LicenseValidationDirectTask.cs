dfb38f0d394552a6d1aadaeded9368ce00925bd7
Fati Iseni <fati.iseni@pozitrongroup.com>
2025-09-09 00:23:52 +0300

    Resolve package reference DLLs from ResolvedCompileFileDefinitions. (#25)
    * Resolve package reference DLLs from ResolvedCompileFileDefinitions.
    
    * Fix tests.

--- src/NuSeal/LicenseValidationDirectTask.cs
+++ src/NuSeal/LicenseValidationDirectTask.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
+using System;
+using System.Linq;
 
 namespace NuSeal;
 
@@ -8,8 +10,21 @@ public partial class LicenseValidationDirectTask : Task
     [Required]
     public string MainAssemblyPath { get; set; } = "";
 
+    [Required]
+    public ITaskItem[] PackageReferences { get; set; } = Array.Empty<ITaskItem>();
+
+    [Required]
+    public ITaskItem[] ResolvedCompileFileDefinitions { get; set; } = Array.Empty<ITaskItem>();
+
     public override bool Execute()
     {
-        return LicenseValidation.Execute(Log, MainAssemblyPath, NuSealValidationScope.Direct);
+        var directPackageIds = PackageReferences.Select(x => x.ItemSpec).ToArray();
+
+        var directPackageDlls = ResolvedCompileFileDefinitions
+            .Where(x => directPackageIds.Contains(x.GetMetadata("NuGetPackageId")))
+            .Select(x => x.ItemSpec)
+            .ToArray();
+
+        return LicenseValidation.Execute(Log, MainAssemblyPath, directPackageDlls, NuSealValidationScope.Direct);
     }
 }
